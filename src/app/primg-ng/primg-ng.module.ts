8e5f583ec4f390ef4c1bbc280e1f6df36b294d0a
John Rubio <git>
2021-07-18 06:18:50 -0500

    PrimeNg

---
+++ src/app/primg-ng/primg-ng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+
+
+@NgModule({
+  exports: [
+    InputTextModule
+  ]
+})
+export class PrimgNgModule { }
